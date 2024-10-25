5f697713891224b07e4451b794e9ed41bad4ea8a
josh <joshcohen98@gmail.com>
2024-10-25 17:25:32 -0400

    Clean up, implement Int and BigInt
    For each, do 0, 1, eq, lt, pred, succ, add, sub, mul and proofs

--- coq-compat/src/CoqBigInt.v
+++ coq-compat/src/CoqBigInt.v
@@ -1,7 +1,5 @@
 Require Export Coq.ZArith.BinInt.
 Require Import Coq.Strings.String.
-(* Require Import CoqUtil. *)
-(* Require Import Integer. *)
 
 Local Open Scope Z_scope.
 
@@ -10,87 +8,31 @@ Local Open Scope Z_scope.
   to OCaml's Zarith.t.
   Any code using BigInt.t should ONLY use this interface
   to ensure that the extracted code is valid OCaml and does
-  not rely on Coq's Z type*)
+  not rely on Coq's Z type.
+  It is easy to extend this with new functions; we include only
+  a minimum subset for now*)
 
 Definition t : Set := Z.
 Definition zero : t := 0.
 Definition one : t := 1.
 Definition succ : t -> t := Z.succ.
+Definition pred : t -> t := Z.pred.
+Definition add : t -> t -> t := Z.add.
+Definition sub : t -> t -> t := Z.sub.
+Definition mul: t -> t -> t := Z.mul.
+Definition lt : t -> t -> bool := Z.ltb.
 Definition eqb : t -> t -> bool := Z.eqb.
+
+(*For specification*)
+Definition to_Z : t -> Z := id.
+
+(*Specs - We write these clearly here bceause they are 
+  axioms of the OCaml Zarith implementation*)
 
 Lemma eqb_eq (z1 z2: t) : eqb z1 z2 = true <-> z1 = z2.
 Proof. apply Z.eqb_eq. Qed.
 
-(* Definition one : t := 1.
-Definition zero : t := 0.
-Definition add : t -> t -> t := Z.add.
-Definition succ : t -> t := Z.succ.
-Definition pred : t -> t := Z.pred.
-Definition sub : t -> t -> t := Z.sub.
-Definition mul : t -> t -> t := Z.mul.
-Definition eqb : t -> t -> bool := Z.eqb.
-Definition compare : t -> t -> CoqInt.int :=
-  fun x y => compare_to_int (Z.compare x y).
-(*Axiom hash : t -> int.*)
-Definition mul_int : CoqInt.int -> t -> t :=
-  fun i z => Z.mul (Integer.int_val _ i) z.
-Definition lt : t -> t -> bool := Z.ltb.
-Definition is_zero : t -> bool := fun z => Z.eqb z 0.
-Definition pos : t -> bool := fun z => Z.ltb 0 z.
-Definition min : t -> t -> t := fun z1 z2 => Z.min z1 z2.
-Definition pow_int_pos_bigint : CoqInt.int -> t -> t := fun base exp => Z.pow (int63_to_Z base) exp.
-Definition of_int : CoqInt.int -> t := fun i => int63_to_Z i.
-(*TODO: implement this - we don't need a good hash function for Coq*)
-Axiom hash : t -> CoqInt.int.
-(*This function is (presumably) a bit different than
-  OCaml, but the implementation doesn't matter*)
-(*TODO: implement*)
-Axiom to_string : t -> string.
-
-(*Single digit numbers*)
-Definition two : t := 2.
-Definition three : t := 3.
-Definition four : t := 4.
-Definition five : t := 5.
-Definition six : t := 6.
-Definition seven : t := 7.
-Definition eight : t := 8.
-Definition nine : t := 9.
-Definition ten : t := 10.
-
-(*Some small primes*)
-Definition eleven : t := 11.
-Definition thirteen : t := 13.
-Definition seventeen : t := 17.
-Definition nineteen : t := 19.
-
-Definition neg_one : t := -1.
-
-(*For Zmap, we implement these functions manually in OCaml*)
-Definition to_Z : t -> Z := id.
-Definition of_Z: Z -> t := id.
-
-(*OCaml function to_pos - gives positive of (x+1) because
-  we allow 0 values
-  (*Go from [numbits x]-1 down to 0*)
-  let to_pos (x: Z.t) = 
-    let rec build_pos (n: int) (x: Z.t) (base: positive) : positive =
-      if n < 0 then base else
-      let b = Z.testbit x n in
-      if b then build_pos (n-1) (xI base)
-      else build_pos (n-1) (xO base)
-    in build_pos (Z.numbits x - 2) (Z.succ x) xH
-*)
-
-(*Specs - These are axioms of the OCaml int implementation*)
-
 (*Equality corresponds to Leibnitz equality*)
-Definition eqb_eq : forall (x y: t), x = y <-> eqb x y :=
-  fun x y => iff_sym (Z.eqb_eq x y).
-
-Lemma to_Z_inj (t1 t2: t) : to_Z t1 = to_Z t2 -> t1 = t2.
-Proof. auto. Qed.
-
 Lemma zero_spec: to_Z zero = 0.
 Proof. reflexivity. Qed.
 
@@ -100,44 +42,20 @@ Proof. reflexivity. Qed.
 Lemma succ_spec z: to_Z (succ z) = Z.succ (to_Z z).
 Proof. reflexivity. Qed.
 
-Lemma eqb_spec z1 z2: eqb z1 z2 = Z.eqb (to_Z z1) (to_Z z2).
+Lemma pred_spec z: to_Z (pred z) = Z.pred (to_Z z).
 Proof. reflexivity. Qed.
 
-Lemma pred_spec z: to_Z (pred z) = Z.pred (to_Z z).
+Lemma add_spec z1 z2: to_Z (add z1 z2) = to_Z z1 + to_Z z2.
+Proof. reflexivity. Qed.
+
+Lemma sub_spec z1 z2: to_Z (sub z1 z2) = to_Z z1 - to_Z z2.
+Proof. reflexivity. Qed.
+
+Lemma mul_spec z1 z2: to_Z (mul z1 z2) = to_Z z1 * to_Z z2.
+Proof. reflexivity. Qed.
+
+Lemma eqb_spec z1 z2: eqb z1 z2 = Z.eqb (to_Z z1) (to_Z z2).
 Proof. reflexivity. Qed.
 
 Lemma lt_spec z1 z2: lt z1 z2 = Z.ltb (to_Z z1) (to_Z z2).
 Proof. reflexivity. Qed.
-
-(*These are all opaque outside of this file*)
-Global Opaque t.
-Global Opaque zero.
-Global Opaque one.
-Global Opaque add.
-Global Opaque succ.
-Global Opaque pred.
-Global Opaque sub.
-Global Opaque mul.
-Global Opaque eqb.
-Global Opaque compare.
-Global Opaque mul_int.
-Global Opaque lt.
-Global Opaque is_zero.
-Global Opaque pos.
-Global Opaque min.
-Global Opaque pow_int_pos_bigint.
-Global Opaque of_int.
-Global Opaque two.
-Global Opaque three.
-Global Opaque four.
-Global Opaque five.
-Global Opaque six.
-Global Opaque seven.
-Global Opaque eight.
-Global Opaque nine.
-Global Opaque to_Z.
-Global Opaque of_Z.
-Global Opaque eleven.
-Global Opaque thirteen.
-Global Opaque seventeen.
-Global Opaque nineteen. *)
--- coq-compat/src/CoqInt.v
+++ coq-compat/src/CoqInt.v
@@ -14,13 +14,15 @@ End Wordsize_31.
 Module Int31 := Make(Wordsize_31).
 
 Definition int := Int31.int.
-Definition eqb := Int31.eq.
-Definition lt := Int31.lt.
 Definition zero := Int31.zero.
 Definition one := Int31.one.
 Definition succ (i: int) : int := Int31.add one i.
 Definition pred (i: int) : int := Int31.sub i one.
-
+Definition add (i1 i2: int) : int := Int31.add i1 i2.
+Definition sub (i1 i2: int) : int := Int31.sub i1 i2.
+Definition mul (i1 i2: int) : int := Int31.mul i1 i2.
+Definition eqb := Int31.eq.
+Definition lt := Int31.lt.
 
 (*For proofs, should never be used in extracted code*)
 (*Define our own functions; clients should not need to know CompCert ints*)
@@ -29,18 +31,47 @@ Definition to_Z (i: int) : Z := Int31.signed i.
 Definition of_Z (z: Z) : int := Int31.repr z.
 
 Require Import Lia.
-(* Require Import Coq.NArith.BinNat. *)
+
+Lemma to_Z_zero: to_Z zero = 0%Z.
+Proof. reflexivity. Qed.
+
+Lemma to_Z_one: to_Z one = 1%Z.
+Proof. reflexivity. Qed.
 
 (*Signed ints: need assumption; cannot have overflow*)
+Lemma to_Z_add (i1 i2: int):
+  (Int31.min_signed <= to_Z i1 + to_Z i2 <= Int31.max_signed)%Z ->
+  to_Z (add i1 i2) = Z.add (to_Z i1) (to_Z i2).
+Proof.
+  intros Hbound. unfold add, to_Z in *.
+  rewrite Int31.add_signed.
+  rewrite Int31.signed_repr; lia.
+Qed. 
+
 Lemma to_Z_succ (i: int): 
   (to_Z i < Int31.max_signed)%Z ->
   to_Z (succ i) = Z.succ (to_Z i).
 Proof.
-  intros Hbound. unfold succ, to_Z, one in *.
-  rewrite Int31.add_signed.
-  rewrite Int31.signed_one by reflexivity.
-  pose proof (Int31.signed_range i).
-  rewrite Int31.signed_repr; lia.
+  intros Hbound. unfold succ. rewrite to_Z_add; rewrite to_Z_one.
+  - apply Z.add_1_l.
+  - pose proof (Int31.signed_range i). unfold to_Z in *. lia.
+Qed.
+
+Lemma to_Z_sub (i1 i2: int):
+  (Int31.min_signed <= to_Z i1 - to_Z i2 <= Int31.max_signed)%Z ->
+  to_Z (sub i1 i2) = Z.sub (to_Z i1) (to_Z i2).
+Proof.
+  intros Hbound. unfold sub, to_Z in *.
+  rewrite Int31.sub_signed, Int31.signed_repr; lia.
+Qed.
+
+Lemma to_Z_pred (i: int): 
+  (Int31.min_signed < to_Z i)%Z ->
+  to_Z (pred i) = Z.pred (to_Z i).
+Proof.
+  intros Hbound. unfold pred. rewrite to_Z_sub; rewrite to_Z_one.
+  - apply Z.sub_1_r.
+  - pose proof (Int31.signed_range i); unfold to_Z in *; lia.
 Qed.
 
 Lemma to_Z_lt (i1 i2: int):
@@ -52,9 +83,6 @@ Proof.
   destruct (Coqlib.zlt _ _); auto; try contradiction. lia.
 Qed.
 
-Lemma to_Z_zero: to_Z zero = 0%Z.
-Proof. reflexivity. Qed.
-
 Lemma to_Z_eqb i1 i2: 
   eqb i1 i2 = Z.eqb (to_Z i1) (to_Z i2).
 Proof.
@@ -63,53 +91,9 @@ Proof.
   destruct (Coqlib.zeq _ _); auto; contradiction.
 Qed.
 
-Lemma to_Z_pred (i: int): 
-  (Int31.min_signed < to_Z i)%Z ->
-  to_Z (pred i) = Z.pred (to_Z i).
-Proof.
-  intros Hbound. unfold pred, to_Z, one in *.
-  rewrite Int31.sub_signed.
-  rewrite Int31.signed_one by reflexivity.
-  pose proof (Int31.signed_range i).
-  rewrite Int31.signed_repr; lia.
-Qed.
-
 (*Useful in proofs*)
 Lemma min31 : Int31.min_signed = (- 1073741824)%Z.
 Proof. reflexivity. Qed.
 
 Lemma max31 : Int31.max_signed = 1073741823%Z.
 Proof. reflexivity. Qed.
-
-(* Require Import Integer.
-
-Definition int : Type := int63.
-Definition int_eqb : int -> int -> bool := int63_eqb.
-(*NOTE: this is an axiom for OCaml*)
-(*TODO: better way of identifying axioms*)
-Definition int_eqb_eq : forall (i1 i2: int), i1 = i2 <-> int_eqb i1 i2 = true :=
-  int63_eqb_eq.
-
-Definition zero : int := int63_of_Z 0%Z eq_refl.
-Definition one : int := int63_of_Z 1%Z eq_refl.
-Definition two : int := int63_of_Z 2%Z eq_refl.
-Definition five : int := int63_of_Z 5%Z eq_refl.
-Definition neg_one : int := int63_of_Z (-1)%Z eq_refl.
-Definition is_zero (i: int) : bool := int_eqb i zero.
-
-Definition compare_to_int (c: comparison) : int :=
-  match c with
-  | Eq => zero
-  | Lt => neg_one
-  | Gt => one
-  end.
-
-Definition compare (i1 i2: int) : int := 
-  compare_to_int (int63_compare i1 i2).
-
-
-(*Add these as axioms: the Coq code never calls them*)
-Axiom add : int -> int -> int.
-Axiom mult: int -> int -> int.
-(*TODO: switch to Compcert ints*)
-Axiom ge : int -> int -> bool. *)
